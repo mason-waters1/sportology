7ba44a892cd0940e9fa53b72cde3a4417f60235e
mason-waters1 <133762343+mason-waters1@users.noreply.github.com>
2023-05-16 11:01:25 -0400

    Update HSFBRecruitPage.js

--- HSFBRecruitPage.js
+++ HSFBRecruitPage.js
@@ -1,18 +1,11 @@
 <script>
-// This first script gets the program banner info
-// retrieve the URL parameter "id" to determine which team's data to retrieve
 const urlParams = new URLSearchParams(window.location.search);
 const teamId = urlParams.get('id');
-
-// retrieve the data from the Xano API endpoint
 fetch(`https://xcrq-vvid-zxzg.n7c.xano.io/api:7q-Aa9wu/high_school_football_programs/${teamId}`)
   .then(response => response.json())
   .then(data => {
-    // populate the programName field
     const programName = data.programName;
     document.querySelector('#programInfo h1').textContent = programName;
-
-    // populate the city, state, and region fields
     const city = data.city;
     const state = data.state;
     const region = data.region;
@@ -23,7 +16,6 @@ fetch(`https://xcrq-vvid-zxzg.n7c.xano.io/api:7q-Aa9wu/high_school_football_prog
     const hudlLink = newRecruitCard.querySelector('#SAHUDLLink');
     hudlLink.href = `${recruit.hudlHandle}`;
 
-    // populate the team banner image
     const teamBannerUrl = data.teamBanner;
     fetch(teamBannerUrl)
       .then(response => response.blob())
